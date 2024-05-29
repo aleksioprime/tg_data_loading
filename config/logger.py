c7000371ab6da900b4f1b35463f872bdf6b4c7b8
Aleksei Semochkin <aleksioprime@gmail.com>
2024-05-29 18:31:05 +0300

    изменение в создании logs

--- config/logger.py
+++ config/logger.py
@@ -1,5 +1,10 @@
 import logging
+import os
 from logging.handlers import RotatingFileHandler
+
+log_dir = 'logs'
+os.makedirs(log_dir, exist_ok=True)
+log_file = os.path.join(log_dir, "app.log")
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -7,6 +12,6 @@ formatter = logging.Formatter(
     "%(asctime)s %(levelname)-8s [%(filename)-16s:%(lineno)-5d] %(message)s"
 )
 
-fh = RotatingFileHandler("logs/app.log", maxBytes=5000000, backupCount=5)
+fh = RotatingFileHandler(log_dir, maxBytes=5000000, backupCount=5)
 fh.setFormatter(formatter)
 logger.addHandler(fh)
